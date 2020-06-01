5c0902b0ba00b11e7aa83356cd577d409345fe73
00254719 <00254719@uca.edu.sv>
2020-05-31 22:04:27 -0600

    Add: Recuperación

---
+++ Recuperación/Correccion preparcial/Preparcial/Controlador/ControladorPedido.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Preparcial.Controlador
+{
+    public static class ControladorPedido
+    {
+        public static DataTable GetPedidosUsuarioTable(string id)
+        {
+            DataTable pedidos = null;
+
+            try
+            {
+                //Correción: Un enter para que no se saliera de la linea
+                pedidos = ConexionBD.EjecutarConsulta("SELECT p.idPedido, i.nombreArt, p.cantidad, i.precio, " +
+                                                      "(i.precio * p.cantidad) AS total" +
+                                            " FROM PEDIDO p, INVENTARIO i, USUARIO u" +
+                                            " WHERE p.idArticulo = i.idArticulo" +
+                                            " AND p.idUsuario = u.idUsuario" +
+                                            $" AND u.idUsuario = {id}");
+            }
+            //Correción: No se usa ex en ningún momento
+            catch (Exception )
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+
+            return pedidos;
+        }
+
+        public static DataTable GetPedidosTable()
+        {
+            DataTable pedidos = null;
+
+            try
+            {
+                //Correción: Un enter para que no se saliera de la linea
+                pedidos = ConexionBD.EjecutarConsulta("SELECT p.idPedido, i.nombreArt, p.cantidad, i.precio, " +
+                                                      "(i.precio * p.cantidad) AS total" +
+                                                      " FROM PEDIDO p, INVENTARIO i, USUARIO u" +
+                                                      " WHERE p.idArticulo = i.idArticulo" +
+                                                      " AND p.idUsuario = u.idUsuario");
+            }
+            
+            //Correción: No se usa ex en ningún momento
+            catch (Exception )
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+
+            return pedidos;
+        }
+
+        public static void HacerPedido(string idUsuario, string idArticulo, string cantidad)
+        {
+            try
+            {
+                ConexionBD.EjecutarComando("INSERT INTO PEDIDO(idUsuario, idArticulo, cantidad) " +
+                    $"VALUES({idUsuario}, {idArticulo}, {cantidad})");
+            }
+            
+            //Correción: No se usa ex en ningún momento
+            catch (Exception )
+            {
+                MessageBox.Show("Ha ocurrido un error");
+            }
+        }
+    }
+}
---
+++ Recuperación/Correccion preparcial/Preparcial/Modelo/Usuario.cs
@@ -0,0 +1,22 @@
+﻿namespace Preparcial.Modelo
+{
+    public class Usuario
+    {
+        public string IdUsuario { get; set; }
+
+        public string Nombre { get; set; }
+        
+        //Correción: Nombre erroneo (contrasena en vez de contrasenia )
+        public string Contrasena { get; set; }
+
+        public bool Admin { get; set; }
+
+        public Usuario(string idUsuario, string nombre, string contrasena, bool admin)
+        {
+            IdUsuario = idUsuario;
+            Nombre = nombre;
+            Contrasena = contrasena;
+            Admin = admin;
+        }
+    }
+}
